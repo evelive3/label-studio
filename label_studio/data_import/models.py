3d2da80750cef5765614508b42e883ac7c245644
Wayland Wu <27021793@qq.com>
2022-08-09 11:23:01 +0800

    Update models.py
    fix:  Unicode filename cannot export

--- label_studio/data_import/models.py
+++ label_studio/data_import/models.py
@@ -23,7 +23,7 @@ def upload_name_generator(instance, filename):
     project = str(instance.project_id)
     project_dir = os.path.join(settings.MEDIA_ROOT, settings.UPLOAD_DIR, project)
     os.makedirs(project_dir, exist_ok=True)
-    path = settings.UPLOAD_DIR + '/' + project + '/' + str(uuid.uuid4())[0:8] + '-' + filename
+    ath = f'{settings.UPLOAD_DIR}/{project}/{str(uuid.uuid4())[24:]}{pathlib.Path(filename).suffix}'
     return path
 
 
